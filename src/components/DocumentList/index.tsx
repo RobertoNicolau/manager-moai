f4fdaf204f17a1bba5436c292724be0973131672
Roberto <roberto.leonardo.2000@gmail.com>
2022-05-31 17:07:03 -0300

    Chamada na API

--- src/components/DocumentList/index.tsx
+++ src/components/DocumentList/index.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
 import { Container } from "./style";
 
 export function DocumentList() {
+  const [documentData, setDocumentData] = useState([])
+  var cnpj = document.getElementById('cnpj')
+  useEffect(() => {
+    fetch('https://managersaas.tecnospeed.com.br:8081/ManagerAPIWeb/nfce/consulta?CNPJ=32021120000161&GRUPO=32021120000161&Filtro=situacao%3DAUTORIZADA',{
+      method: 'GET',  
+      headers:{
+          'Authorization': 'Basic YWRtaW46QkE1MjIyMzM0QUM4NkEwOUNERDY4MjAyMDMwMEVBNUQ=',
+          'Content-Type': 'application/json',
+          'Accept': '*/*'
+        }
+    })
+    .then(response => {
+      return response;
+    })
+    .then(data => console.log(data))
+  }, []);
+  console.log(documentData)
   return (
     <>
       <Container>
@@ -54,3 +72,4 @@ export function DocumentList() {
     </>
   );
 }
+
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { GiMoai } from 'react-icons/gi'
 export function Header(){
     return(
         <Container>
-            <h1>Manager<strong>Tonha</strong></h1>
+            <h1>Result<strong>Manager</strong></h1>
             <GiMoai />
         </Container>
 
